49abc98ecf638b226e8acafd1a16473cbeeb7232
dow2 <dowedwards@gmail.com>
2022-07-15 12:29:51 -0500

    setup webpack

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+
+const port = 8000;
+
+const distPath = path.resolve(__dirname, '..', 'dist');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(distPath));
+
+app.get('/', () => {
+  console.log('this is get');
+});
+
+app.listen(port, () => {
+  console.log(`listening @ http://127.0.0.1:${port}`);
+});
