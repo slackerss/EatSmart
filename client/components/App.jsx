9099a36af1db6a71afbd6c9d784cdf7892e29353
Erin Williams <erin.cwilliams0@gmail.com>
2022-07-18 12:39:54 -0500

    took dotenv require out of index.jsx

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1>EatSmart</h1>
+      <div>Search component</div>
+      <div>Log component</div>
+      <div>Profile component</div>
+    </div>
+  )
+}
+
+export default App;
