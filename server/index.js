74ab48e980e362652273fd01c2f317563690cbed
Brian <brianchau7979@gmail.com>
2022-07-18 18:39:25 -0500

    api get request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const { Users } = require('./db/index.js');
+const { default: axios } = require('axios');
 
 const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
 const RECIPES_API_ID = process.env.RECIPES_API_ID;
@@ -19,9 +20,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(distPath));
 
-app.get(`/api/nutrition-data?app_id=${NUTRITION_API_ID}&app_key=${NUTRITION_API_KEY}&nutrition-type=logging&ingr=chicken`, (req, res) => {
+app.get('/', (req, res) => {
   console.log('this is get');
 });
+
+app.get('/foodlogger', (req, res) => {
+  axios
+    .get(`https://api.edamam.com/api/nutrition-data?app_id=${NUTRITION_API_ID}&app_key=${NUTRITION_API_KEY}&nutrition-type=logging&ingr=chicken`)
+    .then(data => {
+      const { calories } = data.data;
+      const { FAT, CHOCDF, PROCNT} = data.data.totalNutrients;
+      console.log(`calories: ${calories}, fat: ${FAT.quantity}, carbs:${CHOCDF.quantity}, protein:${PROCNT.quantity}`);
+    })
+})
 
 const server = http.createServer(app);
 
