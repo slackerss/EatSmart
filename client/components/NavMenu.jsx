1245b3dfe66eba6a3d4af3b173b27900d594bc61
Erin Williams <erin.cwilliams0@gmail.com>
2022-07-23 17:35:32 -0500

    Navmenu is functional with Link to Home,Profile and functionality to Logout

--- client/components/NavMenu.jsx
+++ client/components/NavMenu.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 
+import LogoutMenuItem from "./Logout-menuItem.jsx";
 
 function Navmenu() {
+  // determines where to Anchor the element
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl)
+
+  const handleMenuClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  }
+
+const handleMenuClose = () => {
+  setAnchorEl(null);
+}
+ 
+const menuItemStyle = {
+  textDecoration: "none",
+  textDecorationColor: "black"
+}
 
   return (
-    
+    <div>
+    <IconButton
+      id="menu-button"
+      size="large"
+      edge="start"
+      color="inherit"
+      aria-controls={open ? 'Eatsmart-menu' : undefined}
+      aria-haspopup="true"
+      aria-expanded={open ? 'true' : undefined}
+      onClick={handleMenuClick}
+      sx={{ mr: 2 }}
+    >
+      <MenuIcon />
+    </IconButton>
+    <Menu 
+    id="Eatsmart-menu"
+    anchorEl={anchorEl}
+    open={open}
+    onClose={handleMenuClose}
+    MenuListProps={{
+      'aria-labelledby': 'menu-button'
+    }}
+    >
+      <MenuItem onClick={handleMenuClose} >
+        <NavLink to="/" style={menuItemStyle} end>Home</NavLink>
+      </MenuItem>
+
+      <MenuItem onClick={handleMenuClose} >
+      <NavLink to="/profile" style={menuItemStyle} >Profile</NavLink>
+      </MenuItem>
+
+      <LogoutMenuItem />
+    </Menu>
+    </div>
   )
 }
 
@@ -19,6 +69,7 @@ function Navmenu() {
 
 
 
+export default Navmenu;
 
 
 
@@ -30,13 +81,3 @@ function Navmenu() {
 
 
 
-
-{/* <IconButton
-      size="large"
-      edge="start"
-      color="inherit"
-      aria-label="menu"
-      sx={{ mr: 2 }}
-      >
-        <MenuIcon />
-      </IconButton> */}
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginButton from './Login-button.jsx';
+import NavMenu from './NavMenu.jsx';
 
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -11,26 +12,18 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 
-function Navbar () {
+function Navbar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-    <IconButton
-      size="large"
-      edge="start"
-      color="inherit"
-      aria-label="menu"
-      sx={{ mr: 2 }}
-      >
-        <MenuIcon />
-      </IconButton>
-    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>EatSmart</Typography>
-    <Link to='/'>Home </Link>
-    <Link to='/profile'>Profile</Link>
-    <LoginButton />
-      </Toolbar>
+          <NavMenu />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>EatSmart</Typography>
+          <Link to='/'>Home </Link>
+          <Link to='/profile'>Profile</Link>
+          <LoginButton />
+        </Toolbar>
       </AppBar>
     </Box>
   )
